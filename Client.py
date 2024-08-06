1e706cc36c74c1c7b43dbfbd2830f6664963bab5
Louie Brooks <louiewbrooks04@gmail.com>
2024-08-06 16:26:01 +0100

    figured out how to send json

--- Client.py
+++ Client.py
@@ -1,6 +1,10 @@
 import websockets
 import asyncio
 import sys
+import json
+
+
+from CustomExitException import *
 
 # https://stackoverflow.com/questions/58454190/python-async-waiting-for-stdin-input-while-doing-other-stuff by user: user4815162342
 async def ainput(string: str) -> str:
@@ -11,17 +15,22 @@ async def ainput(string: str) -> str:
 
 async def receive(ws):
     while True:
-        received = await ws.recv()
-        received = "\t\t\t" + received
+        received_str = await ws.recv()
+        received_dict = json.loads(received_str)
+        received = f"\t\t\t{received_dict["id"]}: {received_dict["message"].strip()}"
         print(received)
 
 async def send(ws):
     while True:
         toSend = await ainput("")
-        if toSend == "exit":
-            raise websockets.AbortHandshake()
+        if toSend.strip() == "exit":
+            raise CustomExitException()
         
-        await ws.send(toSend)
+        send_dict = {
+            "message": toSend,
+            "id": "1"
+        }
+        await ws.send(json.dumps(send_dict))
 
  
 # The main function that will handle connection and communication
@@ -30,20 +39,20 @@ async def ws_client():
 
     try:
         
-        url = "ws://127.0.0.1:7890"
+        url = "ws://127.0.0.1:8765"
         # Connect to the server
         async with websockets.connect(url) as ws:
             
             receieved = await ws.recv()
-            print(f'WebSocket: Client connection status: {receieved}')
-            
+            print(receieved)
+            print("------------------------------------")
             
             await asyncio.gather(
                 receive(ws),
                 send(ws)
             )
             
-    except websockets.AbortHandshake:
+    except CustomExitException:
         print("--------------------------------------------")
         print("You have closed your connection to the server")
     except websockets.ConnectionClosedOK:
--- Server.py
+++ Server.py
@@ -1,6 +1,9 @@
 import websockets
 import asyncio
 import sys
+import json 
+
+from CustomExitException import *
  
 # https://stackoverflow.com/questions/58454190/python-async-waiting-for-stdin-input-while-doing-other-stuff by user: user4815162342
 async def ainput(string: str) -> str:
@@ -11,34 +14,47 @@ async def ainput(string: str) -> str:
  
 async def receive(websocket):
     while True:
-        received = await websocket.recv()
-        received = "\t\t\t" + received
+        received_str = await websocket.recv()
+        received_dict = json.loads(received_str)
+        
+        received = f'\t\t\t{received_dict["id"]}: {received_dict["message"].strip()}'
         print(received)
+
+# FOR THE CONNECTION MANAGER, ON FIRST CONNECTION THE SERVER ASSIGNS AN ID, THEN THE USER MUST COMMUNICATE THIS ID EVERY TIME THEY SEND A MESSAGEs
 
 async def send(websocket):
     while True:
-        toSend = await ainput("")
-        if toSend == "exit":
-            raise websockets.AbortHandshake()
+        messageToSend = await ainput("")
+        if messageToSend.strip() == "exit":
+            raise CustomExitException()
         
-        await websocket.send(toSend)
+        toSend_dict = {
+            "message" : messageToSend.strip(),
+            "id": "Server"
+        }
+        await websocket.send(json.dumps(toSend_dict))
 
 # Creating WebSocket server
 async def ws_server(websocket):
-    print("WebSocket: Server started and Client connected.")
-    await websocket.send("Client connected")
+    print("Conncection to client established")
+    print("------------------------------------")
+    await websocket.send("Connected to server") # let client know its in
+    # await websocket.send({})
     try:
         await asyncio.gather(
             receive(websocket),
             send(websocket)
             )
-    except websockets.AbortHandshake:
-        print("Connection closed")
-    
+    except CustomExitException:
+        print("------------------------------------")
+        print("Connection closed as per request")
+    except websockets.exceptions.ConnectionClosedOK:
+        print("------------------------------------")
+        print("Socket closed as client disconnected")
     
  
 async def main():
-    async with websockets.serve(ws_server, "localhost", 7890):
+    async with websockets.serve(ws_server, "localhost", 8765):
         await asyncio.Future()  # run forever
  
 # basically __name__ is a special method which changes relative to how the module is run, if directly then it is __main__, else it is set to be the name of the file
