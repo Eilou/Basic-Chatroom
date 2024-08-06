6bcbe880170ca778c9df879003a07042c5e85815
Louie Brooks <louiewbrooks04@gmail.com>
2024-08-06 09:45:48 +0100

    initial commit with server and client communications set up

---
+++ Client.py
@@ -0,0 +1,53 @@
+import websockets
+import asyncio
+import sys
+
+# https://stackoverflow.com/questions/58454190/python-async-waiting-for-stdin-input-while-doing-other-stuff by user: user4815162342
+async def ainput(string: str) -> str:
+    await asyncio.get_event_loop().run_in_executor(
+            None, lambda s=string: sys.stdout.write(s+' '))
+    return await asyncio.get_event_loop().run_in_executor(
+            None, sys.stdin.readline)
+
+async def receive(ws) -> None:
+    while True:
+        received = await ws.recv()
+        received = "\t\t\t" + received
+        print(received)
+
+async def send(ws) -> None:
+    while True:
+        toSend = await ainput("")
+        if toSend == "exit":
+            raise websockets.AbortHandshake()
+        
+        await ws.send(toSend)
+
+ 
+# The main function that will handle connection and communication
+# with the server
+async def ws_client():
+
+    try:
+        
+        url = "ws://127.0.0.1:7890"
+        # Connect to the server
+        async with websockets.connect(url) as ws:
+            
+            receieved = await ws.recv()
+            print(f'WebSocket: Client connection status: {receieved}')
+            
+            
+            await asyncio.gather(
+                receive(ws),
+                send(ws)
+            )
+            
+    except websockets.AbortHandshake:
+        print("--------------------------------------------")
+        print("You have closed your connection to the server")
+    except websockets.ConnectionClosedOK:
+        print("Cannot connect to server")
+ 
+# Start the connection
+asyncio.run(ws_client())
---
+++ Server.py
@@ -0,0 +1,46 @@
+import websockets
+import asyncio
+import sys
+ 
+# https://stackoverflow.com/questions/58454190/python-async-waiting-for-stdin-input-while-doing-other-stuff by user: user4815162342
+async def ainput(string: str) -> str:
+    await asyncio.get_event_loop().run_in_executor(
+            None, lambda s=string: sys.stdout.write(s+' '))
+    return await asyncio.get_event_loop().run_in_executor(
+            None, sys.stdin.readline)
+ 
+async def receive(websocket):
+    while True:
+        received = await websocket.recv()
+        received = "\t\t\t" + received
+        print(received)
+
+async def send(websocket):
+    while True:
+        toSend = await ainput("")
+        if toSend == "exit":
+            raise websockets.AbortHandshake()
+        
+        await websocket.send(toSend)
+
+# Creating WebSocket server
+async def ws_server(websocket):
+    print("WebSocket: Server started and Client connected.")
+    await websocket.send("Client connected")
+    try:
+        await asyncio.gather(
+            receive(websocket),
+            send(websocket)
+            )
+    except websockets.AbortHandshake:
+        print("Connection closed")
+    
+    
+ 
+async def main():
+    async with websockets.serve(ws_server, "localhost", 7890):
+        await asyncio.Future()  # run forever
+ 
+# basically __name__ is a special method which changes relative to how the module is run, if directly then it is __main__, else it is set to be the name of the file
+if __name__ == "__main__":
+    asyncio.run(main())
