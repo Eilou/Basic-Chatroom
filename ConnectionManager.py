7242bf905ba21b1a69ca59a5801d690c82d44532
Louie Brooks <louiewbrooks04@gmail.com>
2024-08-09 17:04:54 +0100

    refactored connection manager into a class rather than random functions, for some reason the checkRoomExists method won

--- ConnectionManager.py
+++ ConnectionManager.py
@@ -1,5 +1,9 @@
 # this will hold all the different connections and rooms to enable data transfer
 
+import websockets
+
+from MalformedCommandException import *
+
 class ConnectionManager:
 
     def __init__(self, room_count : int) -> None:
@@ -8,22 +12,7 @@ class ConnectionManager:
         self.names = {} # {"user_id" : "name"}
         
         self.rooms = {}
-        for i in range (room_count):
-            self.rooms[str(i+1)] = set()
-
-    def resetRooms(self) -> None:
-        self.rooms = {}
-    
-    def resetConnections(self, room_count=10) -> None:
-        for i in range (room_count):
-            self.rooms[str(i+1)] = set()
-    
-    def resetServer(self) -> None:
-        self.resetRooms()
-        self.resetConnections()
-
-    def getRoomMembers(self, room_id) -> set:
-        return self.rooms[room_id]
+        self.resetRooms() # default is 10
     
     def addToRoom(self, room_id, user_id) -> None:
         self.rooms[room_id].add(user_id)
@@ -40,6 +29,59 @@ class ConnectionManager:
         for user_id in self.rooms[room_id]:
             await self.connections[user_id].close()
 
+    def removeFromRoom(self, room_id, user_id) -> None:
+        self.rooms[room_id].remove(user_id)
+
+    # the below 2 could maybe be refactored into other functions
+    def checkRoomExists(self, room_id):
+        try:
+            targetRoom = self.rooms[room_id] # this will raise a key error if doesn't exist
+        except KeyError:
+            raise MalformedCommandException(f'Room {room_id} does not exist')
+
+    def checkUserExists(self, user_id):
+        try:
+            target_connection = self.connections[user_id]
+        except KeyError:
+            raise MalformedCommandException(f'User {user_id} does not exist')
+
+
+
+    ######################################
+    ## getters and setters (and resets) ## 
+    ######################################
+
+    def getConnections(self) -> dict:
+        return self.connections
+    
+    def getUserConnection(self, user_id) -> websockets.WebSocketServerProtocol:
+        return self.connections[user_id]
+
+    def getRooms(self) -> dict:
+        return self.rooms
+
+    def getName(self, user_id) -> str:
+        return self.names[user_id]
+
     def setName(self, user_id, name) -> None:
         self.names[user_id] = name
+
+    def resetRooms(self, room_count=10) -> None:
+        for i in range (room_count):
+            self.rooms[str(i+1)] = set()
+
+    async def resetConnections(self) -> None:
+        for user_id in self.connections:
+            await self.getUserConnection(user_id).close()
+        self.connections = {}
+    
+    async def resetServer(self) -> None:
+        self.resetRooms()
+        self.resetConnections()
+
+    def getRoomMembers(self, room_id) -> set:
+        return self.rooms[room_id]
+    
+    def getNextUserID(self) -> str:
+        return str(len(self.connections)+1)
     
--- Server.py
+++ Server.py
@@ -6,25 +6,29 @@ import json
 from CustomExitException import *
 from MalformedCommandException import *
  
-connections = {} # id: websocket object
 
-rooms = {} # room id : set of websocket ids
-for i in range (10):
-    rooms[str(i+1)] = set()
+from ConnectionManager import *
+
+connectionManager = ConnectionManager(10) 
+# connections = {} # id: websocket object
+
+# rooms = {} # room id : set of websocket ids
+# for i in range (10):
+#     rooms[str(i+1)] = set()
 
 
 # think this works but not 100% sure if this is how global stuff works
-def resetConnections():
-    global connections
-    connections = {}
+# def resetConnections():
+#     global connections
+#     connections = {}
 
 
-def resetRooms():
-    global rooms
+# def resetRooms():
+#     global rooms
 
-    rooms = {}
-    for i in range (10):
-        rooms[str(i+1)] = set()
+#     rooms = {}
+#     for i in range (10):
+#         rooms[str(i+1)] = set()
 
 # https://stackoverflow.com/questions/58454190/python-async-waiting-for-stdin-input-while-doing-other-stuff by user: user4815162342
 async def ainput(string: str) -> str:
@@ -34,7 +38,7 @@ async def ainput(string: str) -> str:
             None, sys.stdin.readline)
  
 # receive and forward the message on
-async def receive(websocket):
+async def receive(websocket : websockets.WebSocketServerProtocol, connectionManager : ConnectionManager):
     while True:
         received_str = await websocket.recv()
         received_dict = json.loads(received_str)
@@ -45,24 +49,24 @@ async def receive(websocket):
         # find out which room the user who send the message was in and then room broadcast it there
         room_to_send = received_dict["room_id"]
 
-        await room_broadcast(received_str, room_to_send, received_dict["user_id"])
+        await room_broadcast(connectionManager, received_str, room_to_send, received_dict["user_id"])
         
 # send a message to all in every room
-async def broadcast(message, senderid):
-    for connection_id in connections:
-        if connection_id != senderid:
-            await connections[connection_id].send(message)
+async def broadcast(connectionManager : ConnectionManager, message : dict, sender_id : str):
+    for user_id in connectionManager.getConnections():
+        if user_id != sender_id:
+            await connectionManager.getUserConnection(user_id).send(message)
 
 # send a message to all in a specific room
-async def room_broadcast(message, room_id, senderid):
-    users_in_room = rooms[room_id]
+async def room_broadcast(connectionManager : ConnectionManager, message : dict, room_id : str, sender_id : str):
+    users_in_room = connectionManager.getRoomMembers(room_id)
     for user_id in users_in_room:
-        if user_id != senderid:
-            await connections[user_id].send(message)
+        if user_id != sender_id:
+            await connectionManager.getUserConnection(user_id).send(message)
     
     
 # holds the commands possible to be ran by the server
-async def commands(connections, rooms: set, message: str):
+async def commands(connectionManager : ConnectionManager, message: str):
     message = message[1:].strip()
     command = message
     if message.find("/") != -1: 
@@ -71,11 +75,8 @@ async def commands(connections, rooms: set, message: str):
 
     match command:
 
-        case "removeAll": #/removeAll
-            for connection_id in connections:
-                resetConnections()
-                resetRooms()
-                await connections[connection_id].close()
+        case "reset": #/reset
+            await connectionManager.resetServer()
             raise CustomExitException()
 
         case "broadcast": #/broadcast/message Inserted
@@ -87,7 +88,7 @@ async def commands(connections, rooms: set, message: str):
                 "message" : message[1],
                 "user_id": "Server"
             }
-            await broadcast(json.dumps(toSend_dict), -1) # -1 to indicate server broadcast to all
+            await broadcast(connectionManager ,json.dumps(toSend_dict), -1) # -1 to indicate server broadcast to all
 
         case "rbroadcast": #/rbroadcast/target_room/message Inserted
         
@@ -95,55 +96,54 @@ async def commands(connections, rooms: set, message: str):
                 raise MalformedCommandException("/rbroadcast requires 3 arguments")
             
             target_room_id = message[1].strip()
+            connectionManager.checkRoomExists(target_room_id)
 
-            try:
-                target_room = rooms[target_room_id] # exists to check that the room does actually exist and raise an error if not
-                toSend_dict = {
-                    "message" : message[2],
-                    "user_id": "Server"
-                }
-                await room_broadcast(json.dumps(toSend_dict), target_room_id, -1) 
+            toSend_dict = {
+                "message" : message[2],
+                "user_id": "Server"
+            }
+            await room_broadcast(connectionManager, json.dumps(toSend_dict), target_room_id, -1) 
 
-            except KeyError:
-                raise MalformedCommandException("Target room does not exist")
+            
     
         case "individual": # /individual/2/message Inserted
             if len(message) != 3: 
                 raise MalformedCommandException("/individual requires 3 arguments")
             
             target_user_id = message[1].strip()
+            connectionManager.checkUserExists(target_user_id)
 
-            try:
-                target_connection = connections[target_user_id]
-                toSend_dict = {
-                    "message" : message[2],
-                    "user_id": "Server"
-                }
-                await target_connection.send(json.dumps(toSend_dict))
+            toSend_dict = {
+                "message" : message[2],
+                "user_id": "Server"
+            }
+            await connectionManager.getUserConnection(target_user_id).send(json.dumps(toSend_dict))
 
-            except KeyError:
-                raise MalformedCommandException("Target user is not connected")
 
         case _:
             raise MalformedCommandException("Unknown command")
 
 # server send means broadcast to all
-async def send():
+async def send(connectionManager : ConnectionManager):
     while True:
         message = await ainput("")
         
         # the below section should probably be reworked
         try:
             if message[0] == "/":
-                await commands(connections, rooms, message)
+                await commands(connectionManager, message)
+
         except MalformedCommandException as e:
             print(f"{e.message}")
                     
 
 # Creating WebSocket server
 async def ws_server(websocket):
-    user_id = str(len(connections)+1)
-    connections[user_id] = websocket
+
+    global connectionManager
+
+    user_id = connectionManager.getNextUserID()
+    connectionManager.connections[user_id] = websocket
     
     print(f"Conncection to client {user_id} established")
     print("------------------------------------")
@@ -151,16 +151,16 @@ async def ws_server(websocket):
     await websocket.send("Connected to server") # let client know its in
     await websocket.send(str(user_id)) # let the client know which id to transmit messages as    
 
-    await websocket.send(f'Which room do you want to be placed in (1 - {len(rooms)}): ')
+    await websocket.send(f'Which room do you want to be placed in (1 - {len(connectionManager.getRooms())}): ')
     room_id = await websocket.recv()
-    rooms[room_id].add(user_id)
+    connectionManager.addToRoom(room_id, user_id)
     
     # if a message has to be sent to a specific client put it in the above block before the await
     
     try:
         await asyncio.gather(
-            receive(websocket),
-            send()
+            receive(websocket, connectionManager),
+            send(connectionManager)
             )
     
     except CustomExitException:
